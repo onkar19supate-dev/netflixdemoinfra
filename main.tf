31f979353a40f5cc5d78fc42391dcf42d3deef4a
RAHAMSHAIK <81603735+RAHAMSHAIK007@users.noreply.github.com>
2025-10-06 19:35:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ region = "ap-south-1"
 
 resource "aws_instance" "one" {
 count = 4
-ami = "ami-0f9708d1cd2cfee41"
+ami = "ami-02d26659fd82cf299"
 instance_type = "t2.medium"
 key_name = "demoproject1"
 vpc_security_group_ids = ["sg-0cdd4aaa4687f110c"]
