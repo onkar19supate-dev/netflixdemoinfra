c57ecc370437bb9831e0a7370507008cb5efe42b
RAHAMSHAIK <81603735+RAHAMSHAIK007@users.noreply.github.com>
2025-10-06 19:33:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,12 +3,11 @@ region = "us-east-1"
 }
 
 resource "aws_instance" "one" {
-subnet_id = "subnet-0fd675f95a1ae9990"
 count = 4
-ami = "ami-052064a798f08f0d3"
+ami = "ami-0f9708d1cd2cfee41"
 instance_type = "t2.medium"
-key_name = "saleemkeyp"
-vpc_security_group_ids = ["sg-01ca5f009c7f3b4a0"]
+key_name = "demoproject1"
+vpc_security_group_ids = ["sg-0cdd4aaa4687f110c"]
 tags = {
 Name = var.instance_names[count.index]
 }
