27875707f71e705c909d6c2f3240192d861e041c
Onkar <onkar19supate@gmail.com>
2025-11-09 10:15:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ region = "ap-south-2"
 
 resource "aws_instance" "one" {
 count = 4
-ami = "ami-0bd4cda58efa33d23"
+ami = "ami-0e7938ad51d883574"
 instance_type = "t3.small"
 key_name = "OnkarHYDkeypair"
 vpc_security_group_ids = ["sg-0067493f601890902"]
